22e90e387a3ef1357fe250482a48e33d02089ade
Klaudia Kurela <klakure97@gmail.com>
2023-01-21 12:39:46 +0100

    Added guest functions

--- guests.php
+++ guests.php
@@ -1,73 +1,178 @@
 <?php
-    $title = "Goście";
-    $page = "guests";
-    include_once('header.php');
-    include('functions.php');
+$title = "Goście";
+$page = "guests";
+include_once('header.php');
+include('functions.php');
+session_start();
+session_destroy();
 
-    function show_guest(){
-        global $connection;
+if (empty($_SESSION)) {
+    $_SESSION["modalform"] = '';
+    $_SESSION["name"] = '';
+    $_SESSION["surname"] = '';
+    $_SESSION["adult"] = 'T';
+}
+
+function show_guest()
+{
+    global $connection;
+
+    $request = "select * from goscie";
+    $result = mysqli_query($connection, $request);
+    if (!$result)
+        return;
+
+    $headers = array("Imię", "Nazwisko", "Dorosły");
+    ?>
+    <form method='POST'>
+        <h3>Goście</h3>
+        <div style="text-align:right">
+            <input type="submit" class="btn btn-outline-dark" name='button[-1]' value="Dodaj nowego" />
+        </div>
+
+        <table class='table table-striped table-color'>
+            <thead>
+                <tr class="text-center">
+                <?php
+                foreach ($headers as $header)
+                    echo "<th scope='col'>$header</th>";
+
+                echo "<th scope='col'></th></tr></thead><tbody>";
+                while ($row = mysqli_fetch_row($result)) {
+                    echo "<tr class='text-center'>";
+                    foreach ($row as $c => $cell)
+                        if ($c != 0)
+                            echo "<td>$cell</td>";
+                    echo "<td><input type='submit' class='btn btn-outline-dark' name='button[$row[0]]' value='Edytuj'>
+                            <input type='submit' class='btn btn-outline-dark' name='button[$row[0]]' value='Usuń'>
+                        </td>";
+                    print("</tr>");
+                } ?>
+                </tbody>
+        </table>
+    </form>
+
+    <?= mysqli_free_result($result);
+}
+
+
+function edit_guest($no = -1)
+{
+    global $connection;
+
+    if($no != -1) {
+		$command = "select imie, nazwisko, dorosly from goscie where Id=$no;";
+		$row = mysqli_query($connection, $command) or exit("Błąd w zapytaniu: ".$command);
+                
+        $guest = mysqli_fetch_row($row);
+        $_SESSION["name"] = $guest[0];
+        $_SESSION["surname"] = $guest[1];
+        $_SESSION["adult"] = $guest[2];
+	}
+	else {
+        $_SESSION["name"] = '';
+        $_SESSION["surname"] = '';
+        $_SESSION["adult"] = 'T';
+	}
+
+    $_SESSION["modalform"] = 'editGuest';
+}
+
+function save_guest($no)
+{
+    global $connection;
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    isset($_POST['adult']) ? $adult = 'T' : $adult = 'N';
+
+    if($no != -1)
+        $command = "update goscie set imie='$name', nazwisko='$surname', dorosly='$adult' where id=$no;";
+    else $command = "insert into goscie values(null, '$name', '$surname', '$adult');";
     
-        $request = "select * from goscie";	
-        $result = mysqli_query($connection, $request);
-        if(!$result) return;
+    mysqli_query($connection, $command) or exit("Błąd w zapytaniu: ".$command);
     
-        $headers = array("Imię", "Nazwisko", "Dorosły");
-        echo "<form method='POST'>";
-        echo "<h3>Goście</h3>";
-        echo "<table class='table table-striped table-color'>
-                <thead>
-                    <tr align='center'>";
-        foreach($headers as $header) echo "<th scope='col'>$header</th>";
+    header("Location: guests.php");
+}
 
-        echo "<th scope='col'></th></tr></thead><tbody>";
+function delete_guest($no)
+{
+    global $connection;
+	
+	$command = "delete from goscie where id=$no;";		
+	mysqli_query($connection, $command) or exit("Błąd w zapytaniu: $command");
+}
 
-        while($row = mysqli_fetch_row($result)){		
-                echo "<tr align='center'>";
-                foreach($row as $c=>$cell)
-                    if($c != 0) echo "<td>$cell</td>";
-                echo "<td><input type='submit' class='btn btn-outline-dark' name='przycisk[$row[0]]' value='Edytuj'>
-                                        <input type='submit' class='btn btn-outline-dark' name='przycisk[$row[0]]' value='Usuń'></td>";	
-                print("</tr>");		
-        }
-        echo "</tbody></table>";
-        echo "</form>";
-        mysqli_free_result($result);
-    }
+function checkin_guest($no)
+{
+    //TODO: Add logic or change place of this function.
+}
 
 
-    function edit_guest($nr = -1){
+open_connection();
 
-    }
+$option = '';
+if(isset($_POST['button'])) {	
+	$no = key($_POST['button']);
+	$option = $_POST['button'][$no];
+    $_SESSION["no"] = $no;
+}
 
-    function save_guest($nr){
-        
-    }
+switch($option) {
+    case 'Edytuj': edit_guest($no); break;
+    case 'Dodaj nowego': edit_guest(); break;
+    case 'Zapisz': save_guest($no); break;
+    case 'Usuń': delete_guest($no); break;
+    // case 'Zamelduj': zamelduj_goscia($nr); break;
+}
 
-    function delete_guest($nr){
 
-    }
-
-    function checkin_guest($nr){
-
-    }
+show_guest();
+close_connection();
 ?>
 
+</div>
 
-<?php
-    open_connection();
-
-    // switch($polecenie) {
-    //     case 'Edytuj': edytuj_goscia($nr); break;
-    //     case 'Dodaj nowego': edytuj_goscia(); break;
-    //     case 'Zapisz': zapisz_goscia($nr); break;
-    //     case 'Usuń': usun_goscia($nr); break;
-    //     case 'Zamelduj': zamelduj_goscia($nr); break;
-    // }
-
-    show_guest();
-    close_connection();
-?>
-
+<!-- Edit Guest Modal -->
+<div class="modal fade" id="editGuest" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Dane gościa</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+                <form method=POST action=''>
+                    <div class="modal-body">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="nameInput" name="name" placeholder="Imię..." value="<?=$_SESSION["name"]?>">
+                                <label for="nameInput">Imię</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="surnameInput" name="surname" placeholder="Nazwisko..." value="<?=$_SESSION["surname"]?>">
+                                <label for="surnameInput">Nazwisko</label>
+                            </div>
+                            <div class="margin-5px">
+                                <input type="checkbox" class="mb-3" id="adultInput" name="adult" <?php $_SESSION["adult"] == 'T' ? print('checked') : '' ?>/>
+                                <label for="adultInput">Osoba dorosła</label>
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="button" class="btn btn-secondary" data-bs-dismiss="modal" value="Anuluj"/>
+                        <input type="submit" name="button[<?=$no?>]" class="btn btn-primary" value="Zapisz"/>
+                    </div>
+                </form>
+        </div>
     </div>
-    </body>
+</div>
+
+<script>
+    $(document).ready(function () {
+        let value = '<?php echo $_SESSION['modalform']?>';
+        if (value == 'editGuest') {
+            $('#editGuest').modal('show');
+        }
+    });
+</script>
+
+</body>
+
 </html>
--- header.php
+++ header.php
@@ -16,13 +16,19 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
         crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://kit.fontawesome.com/57bf098a3b.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="main-background">
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-dark navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="home.php">Hotel Laguna</a>
-            <div class="collapse navbar-collapse">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link <?= $page == 'home' ? 'active' : ''?>" href="home.php">Strona główna</a>
