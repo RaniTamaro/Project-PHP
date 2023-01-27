1434b00a6a8bfc4b21ffa9599a4d7e86ee258677
EdytaRz <edytarzonc@gmail.com>
2023-01-27 22:43:22 +0100

    Literowki w addEventListener

--- checkin.php
+++ checkin.php
@@ -209,7 +209,7 @@
 				if (start.value)
 					end.min = start.value;
 			}, false);
-			end.addEventLiseter('change', function() {
+			end.addEventListener('change', function() {
 				if (end.value)
 					start.max = end.value;
 			}, false);
--- guests.php
+++ guests.php
@@ -305,7 +305,7 @@ close_connection();
 				if (start.value)
 					end.min = start.value;
 			}, false);
-			end.addEventLiseter('change', function() {
+			end.addEventListener('change', function() {
 				if (end.value)
 					start.max = end.value;
 			}, false);
